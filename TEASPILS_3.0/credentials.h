d09176841d40b16e30fdbf0b2f4b4d3f7f25c3a9
Juan Luis Moreno <44777241+juanluismsancho@users.noreply.github.com>
2021-09-16 11:50:55 +0200

    Add files via upload

---
+++ TEASPILS_3.0/credentials.h
@@ -0,0 +1,17 @@
+#ifndef CREDENTIALS_HPP
+#define CREDENTIALS_HPP
+
+//wifi credentials
+
+#define wifi             "your wifi"
+#define wifipass       "your wifi pass"
+
+//thingsboard credentials
+
+#define tokenDevice   "your thingsboard device token"
+#define thingsboardServer   "youur thingsboard server"
+
+//telegram
+
+#define tokenBot "your telegram bot token"
+#endif
